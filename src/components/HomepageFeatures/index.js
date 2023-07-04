59e31194113f604da82b1b17bab0cfd0420be646
Igornodz <igor_lealnodari@hotmail.com>
2023-07-04 13:05:04 -0300

    estilizando e arrumando paddings

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -20,7 +20,7 @@ function HomepageFeature() {
                 <img src="img/calendar.svg" className={styles.appLogoQuadrado} alt="logo" />
               </div>
               <div className={clsx('col col--3')}>
-                <p className={clsx(styles.textoCentro, styles.textSubtitleGray)}>2018-2021</p>
+                <p className={clsx(styles.textoCentro, styles.textSubtitleGray)}>2019-2023</p>
               </div>
             </div>
             <hr></hr>
@@ -28,15 +28,33 @@ function HomepageFeature() {
             <h1 className={styles.textTitle}>EXPERIENCE</h1>
             <h1 className={styles.styleHr}></h1>
             <p className={styles.textTitle2}>FrontEnd Junior</p>
-            <p className={styles.textSubtitle}>Everis NTTDATA</p>
-
+            <p className={styles.textSubtitle}>Everis</p>
             <div className="row">
 
               <div className={clsx('col col--3')}>
                 <img src="img/calendar.svg" className={styles.appLogoQuadrado} alt="logo" />
               </div>
               <div className={clsx('col col--3')}>
-                <p className={styles.textSubtitleGray}>2018-2023</p>
+                <p className={styles.textSubtitleGray}>2018-2021</p>
+              </div>
+              <div className={clsx('col col--3')}>
+                <img src="img/location.svg" className={styles.appLogoQuadrado} alt="logo" />
+              </div>
+              <div className={clsx('col col--3')}>
+                <p className={styles.textSubtitleGray}>São Paulo</p>
+              </div>
+            </div>
+
+            <h1 className={styles.styleHr}></h1>
+            <p className={styles.textTitle2}>FrontEnd Pleno</p>
+            <p className={styles.textSubtitle}>NTTDATA</p>
+            <div className="row">
+
+              <div className={clsx('col col--3')}>
+                <img src="img/calendar.svg" className={styles.appLogoQuadrado} alt="logo" />
+              </div>
+              <div className={clsx('col col--3')}>
+                <p className={styles.textSubtitleGray}>2021-2023</p>
               </div>
               <div className={clsx('col col--3')}>
                 <img src="img/location.svg" className={styles.appLogoQuadrado} alt="logo" />
@@ -55,10 +73,28 @@ function HomepageFeature() {
             <h1 className={styles.textTitle}>SOFT SKILLS</h1>
             <h1 className={styles.styleHr}></h1>
             <p className={styles.textTitle2}>
-              ● Com experiência em projetos internos na empresa e projetos nos clientes, Itau e Bradesco.
-              Tive a oportunidade de trabalhar minhas habilidades sociais, lidando com o PO, equipes de QA, Backend, Mainframe, Bff e Design de telas.
-              Também durante esse tempo no meu ultimo projeto ("Bradesco"), tive a oportunidade de interagir com o cliente em atividades cotidianas como reuniões e resolvendo objetivos com eficiência.
-              Evoluindo a comunicação, empatia, escuta ativa e um bom relacionamento entre ambas as partes ! </p>
+              ● Com experiência em projetos SCRUM nos clientes, Itau e Bradesco.
+              Tive a oportunidade de trabalhar minhas habilidades sociais, lidando com o PO, Gerentes, Techleads e equipes de QA, Backend, Mainframe, Bff e Design de telas(UX).
+              Também durante esse tempo, no meu ultimo projeto ("Bradesco"), tive a oportunidade de interagir com o cliente em atividades cotidianas como reuniões, ritos e cerimonias da metodologia SCRUM e resolvendo objetivos com eficiência sempre trabalhando em grupo com proatividade e comprometimento
+              Evoluindo a comunicação, empatia, escuta ativa e um bom relacionamento entre ambas as partes !
+              </p>
+              <hr></hr>
+
+              <h1 className={styles.textTitle}>SOFT SKILLS COURSES</h1>
+              <h1 className={styles.styleHr}></h1>
+              <div className="row">
+              <div className={clsx('col col--6', styles.noPadding)}>
+                <p className={styles.textSubtitle} >Comunicação Positiva</p>
+                <p className={styles.textSubtitle} >Mapas Mentais</p>
+                <p className={styles.textSubtitle} >Lei Geral de proteção de dados</p>
+                <p className={styles.textSubtitle} >Gestão da Tensão e inteligencia emocional</p>
+                <p className={styles.textSubtitle} >Conceitos basicos de segurança</p>
+                <p className={styles.textSubtitle} >Formação global de complience</p>
+                <p className={styles.textSubtitle} >GDPR</p>
+                <p className={styles.textSubtitle} >Comunicação em equipes</p>
+              </div>
+              </div>
+              
             <h1 className={styles.styleHr}></h1>
           </div>
 
@@ -69,21 +105,21 @@ function HomepageFeature() {
 
             <div className='row'>
 
-              <div className={clsx('col col--3', styles.textoCentro,styles.noPadding)}>
+              <div className={clsx('col col--3', styles.textoCentro,styles.paddingItems)}>
                 <img src="img/github.png" className={styles.appLogo} alt="logo" />
               </div>
 
-              <div className={clsx('col col--3', styles.textoCentro,styles.noPadding)} >
+              <div className={clsx('col col--3', styles.textoCentro,styles.paddingItems)} >
                 <a className={styles.textSubTitle2} href="https://github.com/Igornodari?tab=repositories"> GitHub</a>
               </div>
             </div>
 
             <div className='row'>
-              <div className={clsx('col col--3', styles.textoCentro,styles.noPadding)}>
+              <div className={clsx('col col--3', styles.textoCentro,styles.paddingItems)}>
                 <img src="img/linkedin.png" className={styles.appLogo} alt="logo" />
               </div>
 
-              <div className={clsx('col col--3', styles.textoCentro,styles.noPadding)}>
+              <div className={clsx('col col--3', styles.textoCentro,styles.paddingItems)}>
                 <a className={styles.textSubTitle2} href="https://www.linkedin.com/in/igor-leal-nodari-512b7914a/"> Linkedin</a>
               </div>
             </div>
@@ -155,9 +191,11 @@ function HomepageFeature() {
                 <p className={styles.textSubtitle} >MySql</p>
                 <p className={styles.textSubtitle} >PostMan</p>
                 <p className={styles.textSubtitle} >Jira</p>
+                <p className={styles.textSubtitle} >Bitbucket</p>
                 <p className={styles.textSubtitle} >Sass</p>
                 <p className={styles.textSubtitle} >Bootstrap</p>
                 <p className={styles.textSubtitle} >Ionic</p>
+                <p className={styles.textSubtitle} >Confluence</p>
               </div>
               <div className={clsx('col col--6', styles.noPadding)}>
                 <p className={styles.textSubtitle} >React</p>
@@ -165,6 +203,8 @@ function HomepageFeature() {
                 <p className={styles.textSubtitle} >Android</p>
                 <p className={styles.textSubtitle} >SpringMVC</p>
                 <p className={styles.textSubtitle} >Java</p>
+                <p className={styles.textSubtitle} >AdobeXD</p>
+                <p className={styles.textSubtitle} >Zeplin</p>
                 <p className={styles.textSubtitle} >Kotlin</p>
                 <p className={styles.textSubtitle} >JavaScript</p>
                 <p className={styles.textSubtitle} >TypeScript</p>
@@ -175,8 +215,20 @@ function HomepageFeature() {
               </div>
             </div>
 
-            <div className={styles.styleHr}></div>
+            <hr></hr>
+            <h1 className={styles.textTitle}>TECHNICAL SKILLS COURSES</h1>
+            <h1 className={styles.styleHr}></h1>
 
+            <div className="row">
+              <div className={clsx('col col--6', styles.noPadding)}>
+                <p className={styles.textSubtitle} >Udemy</p>
+                <p className={styles.textSubtitle} >Desenvolvimento Android java</p>
+                <p className={styles.textSubtitle} >Alura</p>
+                <p className={styles.textSubtitle} > Desenvolvimento Android kotlin.</p>
+                <p className={styles.textSubtitle} >Alura</p>
+                <p className={styles.textSubtitle} >Formação Angular 7+</p>
+              </div>
+          </div>
           </div>
         </div>
       </div>
